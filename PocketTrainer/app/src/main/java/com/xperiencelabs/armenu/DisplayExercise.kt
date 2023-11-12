dd6f4851232462211428777cc637e559428b1195
Dylan Revsine <44682153+20revsined@users.noreply.github.com>
2023-11-12 18:10:10 -0500

    Update DisplayExercise.kt

--- PocketTrainer/app/src/main/java/com/xperiencelabs/armenu/DisplayExercise.kt
+++ PocketTrainer/app/src/main/java/com/xperiencelabs/armenu/DisplayExercise.kt
@@ -1,66 +1,127 @@
 package com.xperiencelabs.armenu
 
-import android.content.res.Resources
-import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.google.android.filament.utils.loadTexture
-import com.google.ar.core.Config
-import com.xperiencelabs.armenu.ui.theme.ARMenuTheme
-import com.xperiencelabs.armenu.ui.theme.Translucent
-import dev.romainguy.kotlin.math.Quaternion
-import io.github.sceneview.ar.ARScene
-import io.github.sceneview.ar.node.ArModelNode
-import io.github.sceneview.ar.node.ArNode
-import io.github.sceneview.ar.node.PlacementMode
-/*
-class DisplayExercise {
-    ARMenuTheme
-    {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                val currentModel = remember {
-                    mutableStateOf("bigtest")
-                }
-                ARScreen(currentModel.value)
-                Menu(modifier = Modifier.align(Alignment.BottomCenter)) {
-                    currentModel.value = it
-                }
+import androidx.compose.ui.unit.sp
+import com.google.ar.core.dependencies.i
+import com.xperiencelabs.armenu.ui.theme.Gray88
+import com.xperiencelabs.armenu.ui.theme.HeavenWhite
+import androidx.navigation.NavHostController
+import com.xperiencelabs.armenu.ui.theme.arsenic
+import com.xperiencelabs.armenu.ui.theme.lightBlue
 
+class Exercise(name: String, model: String)
+{
+    var exerciseName = ""
+    var demoModel = ""
+    //var id = 0
+    init
+    {
+        exerciseName = name
+        demoModel = model
+        //id = ID
+    }
+
+    fun getName(): String
+    {
+        return exerciseName
+    }
+
+    fun getModel(): String
+    {
+        return demoModel
+    }
+
+    /*
+    fun getId(): Int
+    {
+        return id
+    }
+     */
+}
+
+class ExerciseMenu(e: Exercise)
+{
+    var exercises = arrayOf(e)
+
+    /*
+    fun getExercise(name: String) : Exercise?
+    {
+        if (exercises.contains(name))
+        {
+            return exercises[name]
+        }
+
+        return Exercise("exercise not found", "", -1)
+        //CHECK
+    }
+     */
+
+    @Composable
+    fun ShowExercises(index: Int, exerciseList: Array<Exercise>, navController: NavHostController) {
+        Column(modifier = Modifier
+            .padding(8.dp, 0.dp, 8.dp, 0.dp)
+            .background(color = if (index % 2 == 0) Gray88 else HeavenWhite)) {
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier= Modifier.fillMaxWidth(1f).background(color = lightBlue)) {
+                Text("Pick an Exercise", color = arsenic, fontSize = 30.sp, textAlign = TextAlign.Center)
+            }
+
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier= Modifier.fillMaxWidth(1f)) {
+                for (exercise: Exercise in exerciseList)
+                {
+                    //Text(exercise.exerciseName, color = lightBlue, fontSize = 30.sp, textAlign = TextAlign.Center)
+                    exercise.exerciseName?.let { Text(it, fontSize = 17.sp, modifier = Modifier.padding(4.dp, 8.dp, 4.dp, 0.dp)) }
+
+                    IconButton(onClick = { navController.navigate("DisplayExercise") }) {
+                        Icon(imageVector = ImageVector.vectorResource(R.drawable.start_exercise_button),
+                            contentDescription = stringResource(R.string.exercise_select), //CHECK change to actual exercise name
+                            modifier = Modifier.scale(1.4f),
+                            //tint = Color.Green
+                        )
+                    }
+
+                }
             }
         }
     }
+
 }
-<<<<<<< Updated upstream:ARCore Demo Code/app/src/main/java/com/xperiencelabs/armenu/DisplayExercise.kt
-=======
- */
+
+@Composable
+fun MainView(exerciseMenu: ExerciseMenu, list: Array<Exercise>, navController: NavHostController)
+{
+    exerciseMenu.ShowExercises(index = 0, exerciseList = list, navController = navController)
+}
 
 /*
-sources: https://stackoverflow.com/a/72898717,
-https://developer.android.com/reference/android/text/Layout.Alignment,
-https://stackoverflow.com/a/22966322,
-https://stackoverflow.com/a/64470532
-
->>>>>>> Stashed changes:PocketTrainer/app/src/main/java/com/xperiencelabs/armenu/DisplayExercise.kt
+sources:
+https://developers.google.com/ml-kit/vision/pose-detection/android,
+https://kotlinlang.org/docs/classes.html#constructors,
+https://kotlinlang.org/docs/properties.html#getters-and-setters,
+https://www.simplilearn.com/tutorials/kotlin-tutorial/guide-to-kotlin-constructors,
+https://kotlinlang.org/docs/arrays.html#when-to-use-arrays,
+https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-map/,
+https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-hash-map/,
+https://stackoverflow.com/questions/44905012/method-hashmapof-in-kotlin,
+https://www.baeldung.com/kotlin/initialize-map,
+https://www.geeksforgeeks.org/searchview-in-android-with-kotlin/,
+https://kotlinlang.org/docs/control-flow.html#for-loops,
+https://developer.android.com/develop/ui/views/components/menus
  */
