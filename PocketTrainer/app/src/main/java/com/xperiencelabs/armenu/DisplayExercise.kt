27179e9f81e92e23aa00cd43406091a4a3ce8bd9
Dylan Revsine <44682153+20revsined@users.noreply.github.com>
2023-11-12 18:10:55 -0500

    Update DisplayExercise.kt

--- PocketTrainer/app/src/main/java/com/xperiencelabs/armenu/DisplayExercise.kt
+++ PocketTrainer/app/src/main/java/com/xperiencelabs/armenu/DisplayExercise.kt
@@ -1,127 +1,250 @@
 package com.xperiencelabs.armenu
 
+import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.layout.RowScopeInstance.align
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.ar.core.dependencies.i
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.google.android.filament.utils.loadTexture
+import com.google.ar.core.Config
+import com.xperiencelabs.armenu.ui.theme.ARMenuTheme
 import com.xperiencelabs.armenu.ui.theme.Gray88
 import com.xperiencelabs.armenu.ui.theme.HeavenWhite
-import androidx.navigation.NavHostController
+import com.xperiencelabs.armenu.ui.theme.Translucent
 import com.xperiencelabs.armenu.ui.theme.arsenic
 import com.xperiencelabs.armenu.ui.theme.lightBlue
+import dev.romainguy.kotlin.math.Quaternion
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.ArNode
+import io.github.sceneview.ar.node.PlacementMode
 
-class Exercise(name: String, model: String)
-{
-    var exerciseName = ""
-    var demoModel = ""
-    //var id = 0
-    init
-    {
-        exerciseName = name
-        demoModel = model
-        //id = ID
-    }
+class DisplayExercise : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
 
-    fun getName(): String
-    {
-        return exerciseName
-    }
+            ARMenuTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()){
+                        val currentModel = remember {
+                            mutableStateOf("bigtest")
+                        }
+                        //ARScreen(currentModel.value)
+                        Menu(modifier = Modifier.align(Alignment.BottomCenter)){
+                            currentModel.value = it
+                        }
 
-    fun getModel(): String
-    {
-        return demoModel
-    }
-
-    /*
-    fun getId(): Int
-    {
-        return id
-    }
-     */
-}
-
-class ExerciseMenu(e: Exercise)
-{
-    var exercises = arrayOf(e)
-
-    /*
-    fun getExercise(name: String) : Exercise?
-    {
-        if (exercises.contains(name))
-        {
-            return exercises[name]
-        }
-
-        return Exercise("exercise not found", "", -1)
-        //CHECK
-    }
-     */
-
-    @Composable
-    fun ShowExercises(index: Int, exerciseList: Array<Exercise>, navController: NavHostController) {
-        Column(modifier = Modifier
-            .padding(8.dp, 0.dp, 8.dp, 0.dp)
-            .background(color = if (index % 2 == 0) Gray88 else HeavenWhite)) {
-            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier= Modifier.fillMaxWidth(1f).background(color = lightBlue)) {
-                Text("Pick an Exercise", color = arsenic, fontSize = 30.sp, textAlign = TextAlign.Center)
-            }
-
-            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier= Modifier.fillMaxWidth(1f)) {
-                for (exercise: Exercise in exerciseList)
-                {
-                    //Text(exercise.exerciseName, color = lightBlue, fontSize = 30.sp, textAlign = TextAlign.Center)
-                    exercise.exerciseName?.let { Text(it, fontSize = 17.sp, modifier = Modifier.padding(4.dp, 8.dp, 4.dp, 0.dp)) }
-
-                    IconButton(onClick = { navController.navigate("DisplayExercise") }) {
-                        Icon(imageVector = ImageVector.vectorResource(R.drawable.start_exercise_button),
-                            contentDescription = stringResource(R.string.exercise_select), //CHECK change to actual exercise name
-                            modifier = Modifier.scale(1.4f),
-                            //tint = Color.Green
-                        )
                     }
-
                 }
             }
+        }
+    }
+}
+
+
+
+@Composable
+fun Menu(modifier: Modifier,onClick:(String)->Unit) {
+    var currentIndex by remember {
+        mutableStateOf(0)
+    }
+
+    /*
+
+    Food("burger",R.drawable.burger),
+        Food("instant",R.drawable.instant),
+        Food("momos",R.drawable.momos),
+        Food("pizza",R.drawable.pizza),
+        Food("ramen",R.drawable.ramen),
+     */
+
+    val itemsList = listOf(
+        Models("demo", R.drawable.demo)
+
+    )
+    fun updateIndex(offset:Int){
+        currentIndex = (currentIndex+offset + itemsList.size) % itemsList.size
+        onClick(itemsList[currentIndex].name)
+    }
+    Row(modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        IconButton(onClick = {
+            updateIndex(-1)
+        }) {
+            Icon(painter = painterResource(id = R.drawable.baseline_arrow_back_ios_24), contentDescription ="previous" )
+        }
+
+        CircularImage(imageId = itemsList[currentIndex].imageId )
+
+        IconButton(onClick = {
+            updateIndex(1)
+        }) {
+            Icon(painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24), contentDescription ="next")
         }
     }
 
 }
 
 @Composable
-fun MainView(exerciseMenu: ExerciseMenu, list: Array<Exercise>, navController: NavHostController)
-{
-    exerciseMenu.ShowExercises(index = 0, exerciseList = list, navController = navController)
+fun CircularImage(
+    modifier: Modifier=Modifier,
+    imageId: Int
+) {
+    Box(modifier = modifier
+        .size(140.dp)
+        .clip(CircleShape)
+        .border(width = 3.dp, Translucent, CircleShape)
+    ){
+        Image(painter = painterResource(id = imageId), contentDescription = null, modifier = Modifier.size(140.dp), contentScale = ContentScale.FillBounds)
+    }
 }
 
+@Composable
+fun ARScreen(model:String, navHostController: NavHostController, context: Context) {
+    val nodes = remember {
+        mutableListOf<ArNode>()
+    }
+    val modelNode = remember {
+        mutableStateOf<ArModelNode?>(null)
+    }
+    val placeModelButton = remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = nodes,
+            planeRenderer = true,
+            onCreate = {arSceneView ->
+                arSceneView.lightEstimationMode = Config.LightEstimationMode.DISABLED
+                arSceneView.planeRenderer.isShadowReceiver = false
+                modelNode.value = ArModelNode(arSceneView.engine,PlacementMode.INSTANT).apply {
+                    loadModelGlbAsync(
+                        glbFileLocation = "models/${model}.glb",
+                        scaleToUnits = 0.8f,
+                        autoAnimate = true,
+                        //CHECK
+                        //Rotation needs to be purposeful, not random, research anchor
+                    ){
+
+                    }
+                    onAnchorChanged = {
+                        placeModelButton.value = !isAnchored
+                    }
+                    onHitResult = {node, hitResult ->
+                        placeModelButton.value = node.isTracking
+                    }
+
+                }
+                nodes.add(modelNode.value!!)
+            },
+            onSessionCreate = {
+                planeRenderer.isVisible = false
+            }
+        )
+
+        if(placeModelButton.value){
+            Column(modifier = Modifier
+                .padding(8.dp, 0.dp, 8.dp, 0.dp)
+                .background(color = HeavenWhite)) {
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier= Modifier.fillMaxWidth(1f)) {
+                    IconButton(onClick = { navHostController.popBackStack() }) {
+                            Icon(imageVector = ImageVector.vectorResource(R.drawable.back_button),
+                                contentDescription = stringResource(R.string.exercise_select), //CHECK change to actual exercise name
+                                modifier = Modifier.scale(1.4f),
+                            )
+                    }
+                }
+                Text("Arm Curl Demonstration", color = arsenic, fontSize = 30.sp)
+            }
+
+            Button(onClick = {
+                //val intent = Intent(context, PoseActivity::class.java)
+                //context.startActivity(intent)
+                navHostController.navigate("PoseView")
+            }, modifier = Modifier.align(Alignment.BottomCenter)) {
+                Text(text = "Try exercise myself")
+            }
+        }
+
+    }
+
+
+    LaunchedEffect(key1 = model){
+        modelNode.value?.loadModelGlbAsync(
+            glbFileLocation = "models/${model}.glb",
+            scaleToUnits = 0.8f
+        )
+        Log.e("errorloading","ERROR LOADING MODEL")
+    }
+
+}
+
+
+data class Models(var name:String,var imageId:Int)
 /*
-sources:
-https://developers.google.com/ml-kit/vision/pose-detection/android,
-https://kotlinlang.org/docs/classes.html#constructors,
-https://kotlinlang.org/docs/properties.html#getters-and-setters,
-https://www.simplilearn.com/tutorials/kotlin-tutorial/guide-to-kotlin-constructors,
-https://kotlinlang.org/docs/arrays.html#when-to-use-arrays,
-https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-map/,
-https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-hash-map/,
-https://stackoverflow.com/questions/44905012/method-hashmapof-in-kotlin,
-https://www.baeldung.com/kotlin/initialize-map,
-https://www.geeksforgeeks.org/searchview-in-android-with-kotlin/,
-https://kotlinlang.org/docs/control-flow.html#for-loops,
-https://developer.android.com/develop/ui/views/components/menus
+class DisplayExercise {
+    ARMenuTheme
+    {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                val currentModel = remember {
+                    mutableStateOf("bigtest")
+                }
+                ARScreen(currentModel.value)
+                Menu(modifier = Modifier.align(Alignment.BottomCenter)) {
+                    currentModel.value = it
+                }
+
+            }
+        }
+    }
+}
+ */
+
+/*
+sources: https://stackoverflow.com/a/72898717,
+https://developer.android.com/reference/android/text/Layout.Alignment,
+https://stackoverflow.com/a/22966322,
+https://stackoverflow.com/a/64470532,
+
  */
